10ba1fc793b7a197015a4688ee86785faf985cf6
dshaw-yozu <dshaw@yozu.co.uk>
2025-12-09 09:08:45 +0000

    Update part1.test.ts

--- src/2025/days/7/part1.test.ts
+++ src/2025/days/7/part1.test.ts
@@ -24,18 +24,16 @@ describe("day 7", () => {
   describe("parseInput", () => {
     it("given the raw input text, should return matrix, height and width", () => {
       const result = parseInput(exampleInput);
-      console.log(result.matrix);
       expect(result.matrix.get(coordToString([7, 0]))).toBe("S");
       expect(result.width).toBe(15);
       expect(result.height).toBe(16);
     });
   });
 
-  describe("simularTacyon", () => {
+  describe("simulateTacyon", () => {
     it("should return the final state of the simulation", () => {
       const { matrix, width, height } = parseInput(exampleInput);
       simulateTacyon(matrix, height, width);
-      console.log(matrix);
       expect(matrix.get(coordToString([7, 1]))).toBe("|");
       expect(width).toBe(15);
       expect(height).toBe(16);
